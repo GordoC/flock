ddd1fedec5dceca5819b58839a4da80b14658ffa
gordonch <gordonch@students.cs.ubc.ca>
2023-04-02 12:20:33 -0700

    added session svg

--- frontend/components/screens/UserSessionsScreen.js
+++ frontend/components/screens/UserSessionsScreen.js
@@ -1,12 +1,23 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import Meetings from '../../assets/meetings.svg'
 
 export default function UserSessionsScreen({ navigation }) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => navigation.navigate('Sessions')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Sessions Screen</Text>
+            <Meetings style={styles.bg}/>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    bg: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 16,
+        bottom: 0,
+        zIndex:0,
+        backgroundAttachment: "fixed"
+    }
+})
