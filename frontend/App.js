5c5983e53d88a9d0c77737f05f8a60dad42cb99a
ryanmckim <mck649@gmail.com>
2023-04-02 03:42:43 -0700

    idk

--- frontend/App.js
+++ frontend/App.js
@@ -1,14 +1,28 @@
 //import { io } from 'socket.io-client'
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 // import React, { useState } from 'react';
-import MainContainer from './components/MainContainer';
-import NewSessions from './components/NewSessions';
+import Landing from './components/auth/Landing';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+const Stack = createStackNavigator();
 export default function App() {
+  // const [msg, setMsg] = useState("blah");
+  // const socket = io("http://localhost:1337")
+  // socket.on("connect", () => {
+  //   setMsg("OKOK")
+  // })
+
+  // fetch("http://localhost:1337/api/accounts/2")
+  //   .then((resp) => resp.json())
+  //   .then((data) => setMsg(data.data.id))
 
   return (
-      <NewSessions/>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Landing'>
+        <Stack.Screen name="Landing" component={Landing} options={{headerShown:false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
